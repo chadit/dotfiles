f5023653b801aca9b3fd0f09aa2a1d42dc504138
Chad England <chadit@gmail.com>
2016-12-08 05:35:19 -0500

    updated misc script to include ssh

--- install_solus_misc.sh
+++ install_solus_misc.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # installs solbuild - https://github.com/solus-project/solbuild
-sudo eopkg up
-sudo eopkg it solbuild
+sudo eopkg up -y
+sudo eopkg it -y solbuild
 sudo solbuild init -u
 sudo solbuild update
 
-
+# install ssh - not included because it comes with a security risk
+# netstat -tulpn
+sudo eopkg it -y openssh-server
+sudo systemctl enable sshd
+sudo systemctl start sshd
 
