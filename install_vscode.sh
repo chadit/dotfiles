e0829715f6f22d1dc6063b402efe79a15043a673
Chad England <chadit@gmail.com>
2016-12-14 19:03:39 -0500

    updated script for use user instead of Hard coded user

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
 # vscode
+func ()
+{
+local SCRIPTUSER=${SUDO_USER}
+
+if test "$SCRIPTUSER" = "" || test "$SCRIPTUSER" = "root"; then
+    	 SCRIPTUSER=${USER}
+    fi
 
 # todo add a check for version
 sudo eopkg rm vscode
 
-cd /home/chadit/Downloads/
+cd /home/${SCRIPTUSER}/Downloads/
 
 VSCODEVER=code-stable-code_1.8.0-1481651903_amd64.tar.gz
 
 # Download the sources if file does not exist
-if [ ! -f /home/chadit/Downloads/${VSCODEVER} ]; then
+if [ ! -f /home/${SCRIPTUSER}/Downloads/${VSCODEVER} ]; then
     sudo wget https://az764295.vo.msecnd.net/stable/38746938a4ab94f2f57d9e1309c51fd6fb37553d/${VSCODEVER}
 fi
 
@@ -35,12 +43,13 @@ Actions=new-window;
 Keywords=vscode;"
 
 sudo touch /usr/share/applications/code.desktop
-sudo chown chadit /usr/share/applications/code.desktop
+sudo chown ${SCRIPTUSER} /usr/share/applications/code.desktop
 sudo echo "${VSCODESHORTCUT}" > /usr/share/applications/code.desktop
 
 sudo rm -rf vscode*
 sudo rm -rf VSCode*
 sudo rm -rf code-stable*
-
+}
+func
 
 
