b7706043e29ecd5278b05409bd6b229edc6d10c3
Chad England <chadit@gmail.com>
2023-12-28 12:17:01 -0500

    cleanup copilot

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -14,32 +14,6 @@ function M.new()
           suggestion = { enabled = false },
           panel = { enabled = false },
         })
-
-        -- reenable this if deside later to not use cmp
-        -- require("copilot").setup({
-        --   panel = {
-        --     enabled = true,
-        --     auto_refresh = true,
-        --   },
-        --   suggestion = {
-        --     enabled = true,
-        --     auto_trigger = true,
-        --     accept = false, -- disable built-in keymapping
-        --   }
-        -- })
-
-        -- hide copilot suggestions when cmp menu is open
-        -- to prevent odd behavior/garbled up suggestions
-        -- local cmp_status_ok, cmp = pcall(require, "cmp")
-        -- if cmp_status_ok then
-        --   cmp.event:on("menu_opened", function()
-        --     vim.b.copilot_suggestion_hidden = true
-        --   end)
-
-        --   cmp.event:on("menu_closed", function()
-        --     vim.b.copilot_suggestion_hidden = false
-        --   end)
-        -- end
       end,
     },
 
